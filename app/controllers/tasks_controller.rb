d155cd61cbb150f207f880ee7333eb8bd6c7ee8f
Basant Morsi <basant179@gmail.com>
2023-11-21 16:32:50 +0100

    delete task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,6 +28,13 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+
+  end
+
   private
 
   def tasks_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "tasks", to: "tasks#index", as: :tasks
   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  delete "tasks/:id", to: "tasks#destroy", as: :delete
   patch "tasks/:id", to: "tasks#update", as: :task
   get "tasks/new", to: "tasks#new", as: :new_task
   get "tasks/:id", to: "tasks#show" ,as: :task_id
